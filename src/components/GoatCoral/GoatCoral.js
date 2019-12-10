9ecf2d77b35239109a773910124b6387309ec24a
Monica Djunaidi <modjun12@gmail.com>
2019-12-09 20:42:36 -0600

    prop types

--- src/components/GoatCoral/GoatCoral.js
+++ src/components/GoatCoral/GoatCoral.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Goat from '../Goat/Goat';
 
+import goatShape from '../../helpers/propz/goatShape';
+
 class GoatCoral extends React.Component {
+  static propTypes = {
+    goats: PropTypes.arrayOf(goatShape.goatShape),
+  }
+
   render() {
     const myGoats = this.props.goats;
     // map loops over array and returns our results right away
